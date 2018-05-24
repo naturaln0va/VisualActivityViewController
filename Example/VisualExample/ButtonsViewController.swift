a055c5fb0877103ae6362b86f4f017aaece7a9fe
Ryan Ackermann <ryan@ackermann.io>
2018-05-23 20:20:27 -0500

    added iPad support to the example project

--- Example/VisualExample/ButtonsViewController.swift
+++ Example/VisualExample/ButtonsViewController.swift
@@ -13,14 +13,14 @@ final class ButtonsViewController: UIViewController {
     private let shareText = "Lorem ipsum dolor sit amet, usu an fugit expetendis referrentur. Assum fuisset volumus duo te, ei ubique inimicus eum, nostrum mandamus mel in. Platonem quaerendum comprehensam et nam, at per exerci aliquip persius."
     private let shareURLString = "https://ackermann.io/about"
     
-    @IBAction func textShareButtonPressed() {
+    @IBAction func textShareButtonPressed(_ sender: UIButton) {
         let vc = VisualActivityViewController(text: shareText)
         vc.previewNumberOfLines = 10
         
-        present(vc, animated: true, completion: nil)
+        presentActionSheet(vc, from: sender)
     }
     
-    @IBAction func urlShareButtonPressed() {
+    @IBAction func urlShareButtonPressed(_ sender: UIButton) {
         guard let url = URL(string: shareURLString) else {
             return
         }
@@ -28,17 +28,17 @@ final class ButtonsViewController: UIViewController {
         let vc = VisualActivityViewController(url: url)
         vc.previewLinkColor = .magenta
 
-        present(vc, animated: true, completion: nil)
+        presentActionSheet(vc, from: sender)
     }
     
-    @IBAction func imageShareButtonPressed() {
+    @IBAction func imageShareButtonPressed(_ sender: UIButton) {
         let vc = VisualActivityViewController(image: #imageLiteral(resourceName: "dog"))
         vc.previewImageSideLength = 160
         
-        present(vc, animated: true, completion: nil)
+        presentActionSheet(vc, from: sender)
     }
     
-    @IBAction func allShareButtonPressed() {
+    @IBAction func allShareButtonPressed(_ sender: UIButton) {
         guard let url = URL(string: shareURLString) else {
             return
         }
@@ -46,6 +46,16 @@ final class ButtonsViewController: UIViewController {
         let items: [Any] = [shareText, url, #imageLiteral(resourceName: "dog")]
         let vc = VisualActivityViewController(activityItems: items, applicationActivities: nil)
         vc.previewNumberOfLines = 10
+        
+        presentActionSheet(vc, from: sender)
+    }
+    
+    private func presentActionSheet(_ vc: VisualActivityViewController, from view: UIView) {
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            vc.popoverPresentationController?.sourceView = view
+            vc.popoverPresentationController?.sourceRect = view.bounds
+            vc.popoverPresentationController?.permittedArrowDirections = [.right, .left]
+        }
         
         present(vc, animated: true, completion: nil)
     }
